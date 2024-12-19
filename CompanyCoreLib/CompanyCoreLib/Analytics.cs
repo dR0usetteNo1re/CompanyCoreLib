5d9ce4d47384aefe873bde1ba8910a8c88e2ac21
dR0usetteNo1re <151258421+dR0usetteNo1re@users.noreply.github.com>
2024-12-20 00:03:28 +0300

    Add files via upload

---
+++ CompanyCoreLib/CompanyCoreLib/Analytics.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyCoreLib
+{
+    public class Analytics
+    {
+        class DateTimeWithCounter
+        {
+            public DateTime DateTimeProp;
+            public int Counter = 0;
+
+            public DateTimeWithCounter(DateTime date) //Создание конструктора
+            {
+                DateTimeProp = date;
+                Counter = 1;
+            }
+        }
+
+        public List<DateTime> PopularMonths(List<DateTime> dates)
+        {
+            var DateTimeWithCounterList = new List<Tuple<DateTime, int>>(); //Временный список объектов "ДатаСоСчётчиком"
+
+            int PreviousYear = DateTime.Now.Year - 1;
+            foreach (DateTime IterDate in dates)
+            {
+                if (IterDate.Year == PreviousYear)
+                {
+                    // Вычисление начала месяца для текущей даты
+                    var DateMonthStart = new DateTime(
+                        IterDate.Year, // Год
+                        IterDate.Month, // Месяц
+                        1, 0, 0, 0); // День
+
+                    // Ищем такую дату во временном списке
+                    var index = DateTimeWithCounterList.FindIndex(item => item.Item1 == DateMonthStart);
+
+                    if (index == -1)
+                    {
+                        DateTimeWithCounterList.Add(new Tuple<DateTime, int>(DateMonthStart, 1));
+                    }
+                    else
+                    {
+                        DateTimeWithCounterList[index] = Tuple.Create(DateTimeWithCounterList[index].Item1,
+                        DateTimeWithCounterList[index].Item2 + 1);
+                    }
+                }
+
+
+            }
+
+            return DateTimeWithCounterList
+                .OrderByDescending(item => item.Item2) // Сорт по убыванию
+                .ThenBy(item => item.Item1) // Сорт по следующему параметру
+                .Select(item => item.Item1) // Выбор нужного
+                .ToList(); // Список
+
+        }
+    }
+
+
+}
---
+++ CompanyCoreLib/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CompanyCoreLib;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args) // Пример работы созданной библиотеки
+        {
+            var analytics = new Analytics();
+
+            var srcDates = new List<DateTime>()
+            {
+                /* 
+                На вход подаю несколько дат для более наглядного примера: 
+                Самым популярным будет ноябрь (11)
+                На втором месте по популярности будет август (8)
+                И декабрь (12) на последнем по популярности месте 
+                */
+
+                new DateTime(2023, 8, 12, 0, 0, 0),
+                new DateTime(2023, 8, 11, 0, 0, 0),
+                new DateTime(2023, 12, 10, 0, 0, 0),
+                new DateTime(2023, 11, 11, 0, 0, 0),
+                new DateTime(2023, 11, 11, 0, 0, 0),
+                new DateTime(2023, 11, 7, 0, 0, 0),
+            };
+
+            var outDates = analytics.PopularMonths(srcDates);
+
+            foreach (var date in outDates)
+            {
+                Console.WriteLine(date.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
